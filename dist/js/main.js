12ce08158b0b3262249c4892e2e1a7ff8edc9e54
hard-phillakt <hard-phillakt@mail.ru>
2019-10-22 18:05:38 +0300

    1. http://localhost:3000/cakegoods.html 2.http://localhost:3000/card.html - нужно js-ом накидать стили.

--- dist/js/main.js
+++ dist/js/main.js
@@ -10369,21 +10369,69 @@ return jQuery;
 var navMenuIconlink = document.querySelectorAll('.nav-menu-icon__link');
 var headerFullMenu = document.querySelector('.header-full-menu');
 
-navMenuIconlink[1].onclick = function(){
+navMenuIconlink[1].onclick = function () {
 
-    if(headerFullMenu.classList[2] == 'close-menu'){
+    if (headerFullMenu.classList[2] == 'close-menu') {
         headerFullMenu.classList.remove('close-menu');
     }
-    
+
 }
 
-navMenuIconlink[0].onclick = function(){
+navMenuIconlink[0].onclick = function () {
 
-    if(headerFullMenu.classList[2] != 'close-menu'){
+    if (headerFullMenu.classList[2] != 'close-menu') {
         headerFullMenu.classList.add('close-menu');
     }
-    
+
 }
 
 // Menu end
-console.log('test');
+
+
+
+
+// Filter Cake-category-type-product
+
+var filterSidebarCatalog__box_ul = document.querySelectorAll('.filter-sidebar-catalog__box_ul span');
+
+filterSidebarCatalog__box_ul.forEach(function (item, i) {
+
+    item.onclick = function () {
+
+        if (item.children[0].classList[2] == 'chek-true') {
+
+            item.children[0].classList.remove('chek-true');
+            item.children.checked = false;
+
+        } else {
+            item.children[0].classList.add('chek-true');
+            item.children[1].checked = true;
+        }
+
+    }
+
+});
+
+// Filter Cake-category-type-product end
+
+
+// 
+
+var filterSidebarCatalogBoxCompilation_ul = document.querySelectorAll('.filter-sidebar-catalog__box-compilation_ul span');
+
+filterSidebarCatalogBoxCompilation_ul.forEach(function (item, i) {
+
+    item.onclick = function () {
+
+        filterSidebarCatalogBoxCompilation_ul.forEach(function (item, i) {
+
+            item.classList.remove('link__active');
+            item.children[0].checked = false;
+        });
+
+        item.classList.add('link__active');
+        item.children[0].checked = true;
+
+    }
+
+})
